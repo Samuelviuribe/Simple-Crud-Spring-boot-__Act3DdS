4c560b3eff639376d2f3a1fde82063cdbda47ecb
Samuelviuribe <vianasamuel828@gmail.com>
2024-11-26 00:00:46 -0500

    Primer commit, desde la branch original

---
+++ src/test/java/actividad3/samuelviana/ejercicio18/Ejercicio18ApplicationTests.java
@@ -0,0 +1,13 @@
+package actividad3.samuelviana.ejercicio18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
