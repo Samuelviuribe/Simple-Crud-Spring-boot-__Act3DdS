d11b72c474a08bf759cba1220e7ad95539d2c15f
Samuelviuribe <vianasamuel828@gmail.com>
2024-11-27 22:41:40 -0500

    step 2

---
+++ src/main/java/actividad3/samuelviana/ejercicio18/modelo/Usuario.java
@@ -0,0 +1,21 @@
+package actividad3.samuelviana.ejercicio18.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author HP
+ */
+@Data
+public class Usuario {
+    private String cedula;
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String username;
+    private String rol;
+    private String telefono;
+    private String estado;
+    
+}
